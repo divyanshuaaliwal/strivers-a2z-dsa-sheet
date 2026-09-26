07922b7c155ddb8cd66a7bb4c42df0cf7d9784a8
divyanshuaaliwal <divyanshuaaliwal@gmail.com>
2026-09-26 16:27:20 +0530

    Updated: Find All Divisors

--- 01_BeginnerProblems/02_BasicMaths/09_Find_All_Divisors.cpp
+++ 01_BeginnerProblems/02_BasicMaths/09_Find_All_Divisors.cpp
@@ -2,22 +2,36 @@
 
 class Solution {
   public:
-    vector<int> getDivisors(int num) {
-        
+
+    vector <int> bruteForce(int num) {
         vector <int> ans;
-    
+        for(int i = 1 ; i <= num ; i++) {
+            if(num % i == 0) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+
+    vector <int> optimal(int num) {
+        vector <int> ans;
         int sqrtNum = sqrt(num);
         for(int i = 1 ; i <= sqrtNum ; i++) {
             if(num % i == 0) {
                 ans.push_back(i);
-                if(i != num / i) {
+                if(i != (num / i)) {
                     ans.push_back(num / i);
                 }
             }
-        } 
-        
+        }
+
         sort(ans.begin(), ans.end());
-        
-        return ans;
+
+        return ans;       
+    }
+
+    vector<int> getDivisors(int num) {
+        // return bruteForce(num);      
+        return optimal(num);
     }
 };
